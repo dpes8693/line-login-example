c6d259c037f295daeb7495f09544d107730617f0
Richard Lin <33016870+dpes8693@users.noreply.github.com>
2023-01-03 01:19:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function loginToLine() {
 
 // POST https://api.line.me/oauth2/v2.1/token
 async function getLineToken() {
-    const url = "https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1234567890&redirect_uri=https%3A%2F%2Fexample.com%2Fauth%3Fkey%3Dvalue&state=12345abcde&scope=profile%20openid&nonce=09876xyz"
+    const url = "https://api.line.me/oauth2/v2.1/token"
     const headers = {
         "Content-Type": "application/json",
         "Accept": "application/json",
